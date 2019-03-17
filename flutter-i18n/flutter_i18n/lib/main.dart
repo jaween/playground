acd7eab3113b9476a2e4500cb37e8139be51c062
Jaween Ediriweera <jaween.ediriweera@gmail.com>
2019-03-17 12:15:31 +1030

    Add basic Flutter app for i18n practice

---
+++ flutter-i18n/flutter_i18n/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Internationalisation',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Language example"),
+      ),
+      body: Center(
+        child: Text("I am writing"),
+      ),
+    );
+  }
+}
