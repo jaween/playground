27cc2aa7fd7f75b1e72a5e6c90f6f77b6e3f7b7c
Jaween Ediriweera <jaween.ediriweera@gmail.com>
2019-06-04 22:57:18 +0930

    Init Flutter Audio Recording app

---
+++ flutter_audio_recording/flutter_audio_recording/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Audio recording"),
+      ),
+      body: Center(
+        child: Text("Hello!")
+      ),
+    );
+  }
+}
+
