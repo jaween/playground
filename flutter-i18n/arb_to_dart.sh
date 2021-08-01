9c867da89f78f2fbb7ff74cbf994dbfb771f30eb
Jaween Ediriweera <jaween.ediriweera@gmail.com>
2021-08-01 17:04:53 +0930

    Delete flutter-i18n

--- flutter-i18n/arb_to_dart.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-pub run intl_translation:generate_from_arb --output-dir=lib/l10n/localizations \
-  --no-use-deferred-loading lib/l10n/my_app_localizations.dart \
-  assets/l10n/*.arb
--- flutter-i18n/generate_arbs.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-pub run intl_translation:extract_to_arb --output-dir=assets/l10n/ \
-  lib/l10n/my_app_localizations.dart
-
