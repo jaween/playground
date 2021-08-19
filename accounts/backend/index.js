028bd5913926056153460ef57bb38a00f86d8b39
Jaween Ediriweera <jaween.ediriweera@gmail.com>
2021-08-19 09:57:52 +0930

    Add endpoind to create users via Firebase Auth

--- accounts/backend/index.js
+++ accounts/backend/index.js
@@ -1,13 +1,38 @@
+var admin = require('firebase-admin');
 var express = require('express');
 let app = express();
 
 app.get('/', function (req, res) {
   const name = process.env.NAME || 'no name';
-  res.send(`hello world!!! process ${process}`);
+  res.send(`Hello world!`);
 });
 
+app.post('/users/create', function (req, res) {
+  auth.createUser({}).then(function (user) {
+    res.send(`Created user ${user.uid}\n`)
+  }).catch(function (e) {
+    console.log(`error ${e}`);
+    res.status(500).send('failed');
+  });
+
+});
+
+const credentials = process.env.DEV_GOOGLE_APPLICATION_CREDENTIALS;
+var firebase;
+if (credentials) {
+  var serviceAccount = require(credentials);
+  firebase = admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+} else {
+  firebase = admin.initializeApp({
+    credential: admin.credential.applicationDefault(),
+  })
+}
+let auth = firebase.auth();
 
 const port = process.env.PORT || 8080;
 app.listen(port, function () {
-  console.log(`hello, listening on port ${port}`);
+  console.log(`Listening on port ${port}`);
 });
+
